209c159a7fa9c1506300761949c186480135cc4c
Reece Dunn <msclrhd@gmail.com>
2010-10-14 08:27:24 +0100

    cainteoir: ensure that espeak_Terminate is called on close.

--- src/libcainteoir/espeak_engine.cpp
+++ src/libcainteoir/espeak_engine.cpp
@@ -29,6 +29,11 @@ public:
 		int samplerate = espeak_Initialize(AUDIO_OUTPUT_PLAYBACK, 0, NULL, 0);
 	}
 
+	~espeak_engine()
+	{
+		espeak_Terminate();
+	}
+
 	void speak(cainteoir::buffer *text)
 	{
 		espeak_Synth(text->begin(), text->size(), 0, POS_CHARACTER, 0, espeakCHARS_UTF8|espeakENDPAUSE, NULL, NULL);
