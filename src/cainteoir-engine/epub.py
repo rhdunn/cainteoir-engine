1aad34b9aea412eb4d69eae25d523c1799c3616b
Reece Dunn <msclrhd@gmail.com>
2010-12-07 09:13:07 +0000

    epub: a simple opf metadata parser.

---
+++ src/cainteoir-engine/epub.py
@@ -0,0 +1,44 @@
+"""Metadata
+
+This module provides the framework for storing and working with metadata in
+the Cainteoir Text-to-Speech engine using RDF.
+"""
+
+# Copyright (C) 2010 Reece H. Dunn
+#
+# This file is part of cainteoir-engine.
+#
+# cainteoir-engine is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cainteoir-engine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cainteoir-engine.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from xml.dom import minidom, Node
+
+import metadata
+
+class OpfMetadata:
+	def __init__(self, filename, subject):
+		self.subject = metadata.RDFResource(subject)
+		self.dom = minidom.parse(filename).documentElement
+
+	def triples(self):
+		for node in self.dom.getElementsByTagName('metadata')[0].childNodes:
+			if node.nodeType == Node.ELEMENT_NODE:
+				predicate = metadata.RDFResource(node.localName, node.namespaceURI)
+				object = metadata.RDFLiteral(node.childNodes[0].nodeValue)
+				yield metadata.RDFTriple(self.subject, predicate, object)
+
+	def format(self, prefixes={}):
+		for triple in self.triples():
+			yield triple.format(prefixes)
+
---
+++ tests/opf-metadata.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+# Copyright (C) 2010 Reece H. Dunn
+#
+# This file is part of cainteoir-engine.
+#
+# cainteoir-engine is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cainteoir-engine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cainteoir-engine.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import os
+
+sys.path.append(os.path.join(sys.path[0], '../src/cainteoir-engine'))
+
+import epub
+import harness as test
+
+def check_metadata(filename, expect):
+	fsbase = '%s/' % os.path.abspath(os.path.join(sys.path[0], 'opf-metadata'))
+	prefixes = {
+		'http://www.idpf.org/2007/opf': 'opf',
+		'http://purl.org/dc/elements/1.1/': 'dc',
+		fsbase: 'fs',
+	}
+
+	filename = os.path.join(sys.path[0], filename)
+	opf = epub.OpfMetadata(filename, filename)
+
+	with open(os.path.join(sys.path[0], expect), 'r') as f:
+		expected = [ unicode(x) for x in f.read().split('\n') if not x == '' ]
+
+	got = [ x for x in opf.format(prefixes) ]
+
+	test.equal(expected, got, 'opf.format(file=%s)' % filename)
+
+if __name__ == '__main__':
+	check_metadata('opf-metadata/dublincore.opf', 'opf-metadata/dublincore.n3')
+
