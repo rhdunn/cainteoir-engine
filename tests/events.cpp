1376edab05aec17d7c7a6b30db5310fb25104146
Reece Dunn <msclrhd@gmail.com>
2011-04-19 20:21:01 +0100

    tests: fix size_t printf warning

--- tests/events.cpp
+++ tests/events.cpp
@@ -44,7 +44,7 @@ struct events : public cainteoir::document_events
 
 	void text(std::tr1::shared_ptr<cainteoir::buffer> aText)
 	{
-		fprintf(stdout, "text(%d): \"\"\"", aText->size());
+		fprintf(stdout, "text(%zu): \"\"\"", aText->size());
 		fwrite(aText->begin(), 1, aText->size(), stdout);
 		fwrite("\"\"\"\n", 1, 4, stdout);
 	}
