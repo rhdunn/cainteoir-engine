ac19edadcc040585142764cde3517b84c3b1acae
Reece Dunn <msclrhd@gmail.com>
2011-07-01 08:15:51 +0100

    ubuntu: support publishing to different ubuntu releases on the ppa

--- build.sh
+++ build.sh
@@ -41,7 +41,30 @@ douninstall() {
 }
 
 doppa() {
-	dput ppa:msclrhd-gmail/cainteoir ../${PACKAGE}_*_source.changes
+	# Ubuntu supports a single upload building on multiple distros, but only if the
+	# package is in bazaar and hosted on Launchpad. The only other way to specify
+	# the target version of Ubuntu to upload to is to specify the distro name in the
+	# changelog.
+	#
+	# Ubuntu actually ignores the distro name when building the package, but the
+	# |dput| command does not.
+	#
+	# In addition to this, it is advised that a version identifier is used for ppa
+	# files, so a "~<distro-name>1" is appended.
+
+	DISTRO=$1
+	sed -i -e "s/) unstable;/~${DISTRO}1) ${DISTRO};/" debian/changelog
+	dodebsrc || (sed -i -e "s/~${DISTRO}1) ${DISTRO};/) unstable;/" debian/changelog && exit 1)
+	sed -i -e "s/~${DISTRO}1) ${DISTRO};/) unstable;/" debian/changelog
+
+	dput ppa:msclrhd-gmail/cainteoir ../${PACKAGE}_*~${DISTRO}1_source.changes
+}
+
+doallppa() {
+	# NOTE: lucid is missing the autopoint package
+	for DISTRO in maverick natty oneiric ; do
+		doppa ${DISTRO}
+	done
 }
 
 case "$1" in
@@ -49,7 +72,8 @@ case "$1" in
 	deb)       dodeb ;;
 	debsrc)    dodebsrc ;;
 	dist)      dodist ;;
-	ppa)       doppa ;;
+	ppa)       doppa $2 ;;
+	allppa)    doallppa ;;
 	release)   dorelease ;;
 	install)   doinstall ;;
 	uninstall) douninstall ;;
@@ -75,9 +99,12 @@ To build and install locally on a Debian system, run:
     `basename $0` deb
     `basename $0` install
 
-To publish to the Ubuntu PPA, run:
+To publish to Debian, run:
     `basename $0` release
-    `basename $0` ppa
+
+To publish to the Ubuntu PPA for a specific distribution, run:
+    `basename $0` release
+    `basename $0` ppa <distro-name>
 "
 		;;
 esac
