9a05cda1a3cd8df36047ca030cd3b84d24de50c6
Reece Dunn <msclrhd@gmail.com>
2011-07-18 11:04:39 +0100

    libcainteoir: espeak volume is expressed as a percentage

--- src/libcainteoir/tts/espeak.cpp
+++ src/libcainteoir/tts/espeak.cpp
@@ -117,7 +117,7 @@ class espeak_engine : public tts::engine
 public:
 	espeak_engine(rdf::graph &metadata, std::string &baseuri)
 		: mRate(new espeak_parameter(_("Speed"), "wpm", espeakRATE, 80, 450, 1))
-		, mVolume(new espeak_parameter(_("Volume"), "", espeakVOLUME, 0, 200, 1))
+		, mVolume(new espeak_parameter(_("Volume"), "%", espeakVOLUME, 0, 200, 1))
 		, mPitch(new espeak_parameter(_("Base Pitch"), "", espeakPITCH, 0, 100, 1))
 		, mPitchRange(new espeak_parameter(_("Pitch Variation"), "", espeakPITCH, 0, 100, 1))
 		, mWordGap(new espeak_parameter(_("Word Gap"), "ms", espeakWORDGAP, 0, 500, 10))
