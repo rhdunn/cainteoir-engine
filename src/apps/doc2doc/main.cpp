21035773fccdfc5cebc29ebdd9d8094e561b1143
Reece Dunn <msclrhd@gmail.com>
2012-05-14 19:54:19 +0100

    doc2doc: switch the document writers to use the reader API.

--- src/apps/doc2doc/main.cpp
+++ src/apps/doc2doc/main.cpp
@@ -26,112 +26,110 @@
 namespace rdf    = cainteoir::rdf;
 namespace events = cainteoir::events;
 
-struct text_printer : public cainteoir::document_events
+void writeTextDocument(std::shared_ptr<cainteoir::document_reader> reader)
 {
-	void text(std::shared_ptr<cainteoir::buffer> aText)
+	while (reader->read())
 	{
-		fwrite(aText->begin(), 1, aText->size(), stdout);
-	}
-
-	void begin_context(events::context aContext, uint32_t aParameter)
-	{
-		switch (aContext)
+		if (reader->type & cainteoir::events::begin_context)
 		{
-		case events::paragraph:
-		case events::heading:
-		case events::list:
-		case events::list_item:
-			fwrite("\n\n", 1, 2, stdout);
-			break;
+			switch (reader->context)
+			{
+			case events::paragraph:
+			case events::heading:
+			case events::list:
+			case events::list_item:
+				fwrite("\n\n", 1, 2, stdout);
+				break;
+			}
+		}
+		if (reader->type & cainteoir::events::text)
+		{
+			fwrite(reader->text->begin(), 1, reader->text->size(), stdout);
 		}
 	}
-};
+}
 
-struct html_printer : public cainteoir::document_events
+void writeHtmlDocument(std::shared_ptr<cainteoir::document_reader> reader)
 {
-	html_printer()
-		: mFirst(true)
-	{
-		fprintf(stdout, "<html>\n");
-		fprintf(stdout, "<body>\n");
-	}
-
-	~html_printer()
-	{
-		fprintf(stdout, "\n</body>");
-		fprintf(stdout, "\n</html>");
-	}
-
-	void text(std::shared_ptr<cainteoir::buffer> aText)
-	{
-		if (!aText->empty())
-			mText += aText;
-	}
-
-	void begin_context(events::context aContext, uint32_t aParameter)
-	{
-		std::pair<std::string, bool> context;
-		switch (aContext)
-		{
-		case events::paragraph: context = { "p", true }; break;
-		case events::heading:
-			switch (aParameter)
-			{
-			case 1:  context = { "h1", true }; break;
-			case 2:  context = { "h2", true }; break;
-			case 3:  context = { "h3", true }; break;
-			case 4:  context = { "h4", true }; break;
-			case 5:  context = { "h5", true }; break;
-			default: context = { "h6", true }; break;
-			}
-			break;
-		case events::span:
-			switch (aParameter)
-			{
-			case events::superscript: context = { "sup", false }; break;
-			case events::subscript:   context = { "sub", false }; break;
-			case events::emphasized:  context = { "em", false }; break;
-			case events::strong:      context = { "strong", false }; break;
-			case events::underline:   context = { "u", false }; break;
-			case events::monospace:   context = { "pre", false }; break;
-			default:                  context = { "span", false }; break;
-			}
-			break;
-		case events::list:      context = { "ol", true }; break;
-		case events::list_item: context = { "li", true }; break;
-		default:                context = { "span", true }; break;
-		}
-
-		if (context.second && !mFirst)
-			fprintf(stdout, "\n<%s>", context.first.c_str());
-		else
-			fprintf(stdout, "<%s>", context.first.c_str());
-		ctx.push(context.first);
-		mFirst = false;
-	}
-
-	void end_context()
-	{
-		if (!mText.empty())
-		{
-			auto norm = mText.normalize();
-			mText.clear();
-
-			if (!norm->empty())
-				fwrite(norm->begin(), 1, norm->size(), stdout);
-		}
-
-		if (!ctx.empty())
-		{
-			fprintf(stdout, "</%s>", ctx.top().c_str());
-			ctx.pop();
-		}
-	}
-
-	bool mFirst;
-	cainteoir::rope mText;
+	bool first = true;
+	cainteoir::rope text;
 	std::stack<std::string> ctx;
-};
+
+	fprintf(stdout, "<html>\n");
+	fprintf(stdout, "<body>\n");
+
+	while (reader->read())
+	{
+		if (reader->type & cainteoir::events::begin_context)
+		{
+			std::pair<std::string, bool> context;
+			switch (reader->context)
+			{
+			case events::paragraph: context = { "p", true }; break;
+			case events::heading:
+				switch (reader->parameter)
+				{
+				case 1:  context = { "h1", true }; break;
+				case 2:  context = { "h2", true }; break;
+				case 3:  context = { "h3", true }; break;
+				case 4:  context = { "h4", true }; break;
+				case 5:  context = { "h5", true }; break;
+				default: context = { "h6", true }; break;
+				}
+				break;
+			case events::span:
+				switch (reader->parameter)
+				{
+				case events::superscript: context = { "sup", false }; break;
+				case events::subscript:   context = { "sub", false }; break;
+				case events::emphasized:  context = { "em", false }; break;
+				case events::strong:      context = { "strong", false }; break;
+				case events::underline:   context = { "u", false }; break;
+				case events::monospace:   context = { "pre", false }; break;
+				default:                  context = { "span", false }; break;
+				}
+				break;
+			case events::list:      context = { "ol", true }; break;
+			case events::list_item: context = { "li", true }; break;
+			default:                context = { "span", true }; break;
+			}
+
+			if (context.second && !first)
+				fprintf(stdout, "\n<%s>", context.first.c_str());
+			else
+				fprintf(stdout, "<%s>", context.first.c_str());
+			ctx.push(context.first);
+			first = false;
+		}
+
+		if (reader->type & cainteoir::events::text)
+		{
+			if (!reader->text->empty())
+				text += reader->text;
+		}
+
+		if (reader->type & cainteoir::events::end_context)
+		{
+			if (!text.empty())
+			{
+				auto norm = text.normalize();
+				text.clear();
+
+				if (!norm->empty())
+					fwrite(norm->begin(), 1, norm->size(), stdout);
+			}
+
+			if (!ctx.empty())
+			{
+				fprintf(stdout, "</%s>", ctx.top().c_str());
+				ctx.pop();
+			}
+		}
+	}
+
+	fprintf(stdout, "\n</body>");
+	fprintf(stdout, "\n</html>");
+}
 
 int main(int argc, char ** argv)
 {
@@ -143,17 +141,17 @@ int main(int argc, char ** argv)
 		if (argc != 2)
 			throw std::runtime_error("no document specified");
 
-		std::shared_ptr<cainteoir::document_events> events;
+		rdf::graph metadata;
+		auto reader = cainteoir::createDocumentReader(argv[1], metadata, std::string());
+		if (!reader)
+			fprintf(stderr, "unsupported document format for file \"%s\"\n", argv[1]);
+
 		if (!strcmp(argv[0], "text"))
-			events = std::make_shared<text_printer>();
+			writeTextDocument(reader);
 		else if (!strcmp(argv[0], "html"))
-			events = std::make_shared<html_printer>();
+			writeHtmlDocument(reader);
 		else
 			throw std::runtime_error("unsupported format to convert to (html and text only)");
-
-		rdf::graph metadata;
-		if (!cainteoir::parseDocument(argv[1], *events, metadata))
-			fprintf(stderr, "unsupported document format for file \"%s\"\n", argv[1]);
 	}
 	catch (std::runtime_error &e)
 	{
