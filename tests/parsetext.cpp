a35f336685d37ec00432ca1f4523a34b2f36b498
Reece Dunn <msclrhd@gmail.com>
2012-06-05 23:14:20 +0100

    parsetext: support ',' characters in numbers

--- tests/parsetext.cpp
+++ tests/parsetext.cpp
@@ -134,6 +134,7 @@ void parseTextBuffer(const std::shared_ptr<cainteoir::buffer> &aText)
 			// number:
 			case '0': case '1': case '2': case '3': case '4':
 			case '5': case '6': case '7': case '8': case '9':
+			case ',':
 				break;
 			default:
 				fputs(".number        ", stdout);
