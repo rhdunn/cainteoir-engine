8aa3db00ab24c71d3c0f5d593464e53bf483c062
Reece H. Dunn <msclrhd@gmail.com>
2012-09-17 18:15:52 +0100

    mkapidocs.py: generate class, struct and namespace pages with navigation

--- docs/mkapidocs.py
+++ docs/mkapidocs.py
@@ -45,7 +45,7 @@ def parseDoxygenId(ref):
 		for match, replace in [(u'__', u'_')]:
 			item = item.replace(match, replace)
 		ret.append(item)
-	return kind, ret
+	return kind, ret[:-1], ret[-1]
 
 class DocItem:
 	def __init__(self, ref, kind, name, compound):
@@ -53,6 +53,7 @@ class DocItem:
 		self.kind = kind
 		self.name = name
 		self.compound = compound
+		_, self.scope, self.shortname = parseDoxygenId(ref)
 
 	def __str__(self):
 		return '%s %s' % (self.kind, self.name)
@@ -61,6 +62,15 @@ class DocCompound(DocItem):
 	def __init__(self, ref, kind, name):
 		DocItem.__init__(self, ref, kind, name, True)
 		self.members = []
+
+class DocClass(DocCompound):
+	def __init__(self, ref, kind, name):
+		DocCompound.__init__(self, ref, kind, name)
+		self.protection = None
+		self.base = []
+
+	def __str__(self):
+		return '%s %s %s' % (self.protection, self.kind, self.name)
 
 class DocMember(DocItem):
 	def __init__(self, ref, kind, name):
@@ -73,7 +83,7 @@ class Documentation:
 	def create(self, ref, kind, name):
 		if ref in self.items.keys():
 			return self.items[ref]
-		if kind == 'class':     self.items[ref] = DocCompound(ref, kind, name)
+		if kind == 'class':     self.items[ref] = DocClass(ref, kind, name)
 		if kind == 'define':    self.items[ref] = DocMember(ref, kind, name)
 		if kind == 'dir':       self.items[ref] = DocCompound(ref, kind, name)
 		if kind == 'enum':      self.items[ref] = DocMember(ref, kind, name)
@@ -82,7 +92,7 @@ class Documentation:
 		if kind == 'function':  self.items[ref] = DocMember(ref, kind, name)
 		if kind == 'namespace': self.items[ref] = DocCompound(ref, kind, name)
 		if kind == 'page':      self.items[ref] = DocCompound(ref, kind, name)
-		if kind == 'struct':    self.items[ref] = DocCompound(ref, kind, name)
+		if kind == 'struct':    self.items[ref] = DocClass(ref, kind, name)
 		if kind == 'typedef':   self.items[ref] = DocMember(ref, kind, name)
 		if kind == 'variable':  self.items[ref] = DocMember(ref, kind, name)
 		if ref not in self.items.keys():
@@ -92,6 +102,27 @@ class Documentation:
 	def __iter__(self):
 		for item in self.items.values():
 			yield item
+
+	def __getitem__(self, ref):
+		return self.items[ref]
+
+def parseDoxygenClass(xmlroot, c, doc):
+	xml = Document(os.path.join(xmlroot, '%s.xml' % c.ref))
+	compound = list(xml.children())[0]
+
+	ref = compound['id']
+	kind = compound['kind']
+	protection = compound['prot']
+
+	if ref != c.ref or kind != c.kind:
+		raise Exception('Class documentation file mismatch for %s' % c.ref)
+
+	c.protection = protection
+	for node in compound.children():
+		if node.name == 'basecompoundref':
+			baseref = node['refid']
+			if baseref != '':
+				c.base.append(doc[baseref])
 
 def parseDoxygenDocumentation(xmlroot):
 	xml = Document(os.path.join(xmlroot, 'index.xml'))
@@ -113,11 +144,40 @@ def parseDoxygenDocumentation(xmlroot):
 						membername = membernode.text()
 						m = doc.create(memberref, memberkind, membername)
 						c.members.append(m)
+
+	for c in doc:
+		if c.kind in ['struct', 'class']:
+			parseDoxygenClass(xmlroot, c, doc)
+		scope = []
+		namespace = []
+		for ns in c.scope:
+			scope.append(ns)
+			for kind in ['namespace', 'class', 'struct']:
+				scopename = '%s%s' % (kind, '_1_1'.join([x.replace('_', '__') for x in scope]))
+				try:
+					value = doc[scopename]
+					namespace.append(value)
+				except KeyError:
+					pass
+		c.scope = namespace
+
 	return doc
 
+docroot = sys.argv[2]
 doc = parseDoxygenDocumentation(sys.argv[1])
 for item in doc:
-	if item.compound:
-		print '%s' % (item)
-		for member in item.members:
-			print '... %s' % (member)
+	if item.kind in ['struct', 'class', 'namespace']:
+		print 'writing %s ...' % item.ref
+		with open(os.path.join(docroot, '%s.html' % item.ref), 'w') as f:
+			title = '%s %s Documentation' % (item.name, item.kind.capitalize())
+			f.write('---\n')
+			f.write('layout: rdfa\n')
+			f.write('title: Cainteoir Text-to-Speech API &mdash; %s\n' % title)
+			f.write('nav:\n')
+			f.write('  - { title: Home , url: ../index.html }\n')
+			f.write('  - { title: API , url: index.html }\n')
+			for s in item.scope:
+				f.write('  - { title: "%s" , url: %s.html }\n' % (s.shortname, s.ref))
+			f.write('  - { title: "%s" }\n' % item.shortname)
+			f.write('---\n')
+			f.write('<h1>%s</h1>\n' % title)
