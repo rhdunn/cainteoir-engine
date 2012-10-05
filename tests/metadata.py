002e3cd0c2b661544a04f3ac2ba0fe775ecacc12
Reece H. Dunn <msclrhd@gmail.com>
2012-10-05 08:45:41 +0100

    epub3: add a test for xml-signature metadata

--- tests/metadata.py
+++ tests/metadata.py
@@ -239,6 +239,7 @@ if __name__ == '__main__':
 			{'test': 'opf/metadata30/marc21xml-record.opf', 'result': 'opf/metadata30/marc21xml-record.n3'},
 			{'test': 'opf/metadata30/mods-record.opf', 'result': 'opf/metadata30/mods-record.n3'},
 			{'test': 'opf/metadata30/onix-record.opf', 'result': 'opf/metadata30/onix-record.n3'},
+			{'test': 'opf/metadata30/xml-signature.opf', 'result': 'opf/metadata30/xml-signature.n3'},
 		]},
 	]})
 	test.run({ 'name': 'ePub', 'replace': ['mimetype'], 'mimetype': 'application/epub+zip',
