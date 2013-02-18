836861e5a64372d56f0ffe436c95f98fc82b6b4c
Reece H. Dunn <msclrhd@gmail.com>
2013-02-18 09:55:23 +0000

    word_stream.cpp: don't use {} syntax when pushing events to the queue

--- src/libcainteoir/tts/word_stream.cpp
+++ src/libcainteoir/tts/word_stream.cpp
@@ -45,7 +45,7 @@ bool tts::word_stream::read()
 			case tts::word_capitalized:
 			case tts::word_mixedcase:
 			case tts::word_script:
-				mEntries.push({ event });
+				mEntries.push(event);
 				break;
 			case tts::end_of_paragraph:
 				end_of_clause = true;
