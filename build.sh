b07cfe1e68941e357d105243a42e9f0b50e68206
Reece H. Dunn <msclrhd@gmail.com>
2013-04-09 16:32:49 +0100

    android: initial configure helper for the Android NDK toolchain

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PACKAGE=cainteoir-engine
+ANDROID_VERSION=8
+ANDROID_COMPILER=4.6
+ANDROID_ARCHS="armeabi armeabi-v7a x86 mips"
 
 doclean() {
 	git clean -fxd
@@ -14,7 +17,18 @@ dodist() {
 		( popd && exit 1 )
 }
 
+doandroid() {
+	./autogen.sh || exit 1
+	for arch in ${ANDROID_ARCHS} ; do
+		echo "===== android ${arch} ====="
+		android/ndk-configure android-${ANDROID_VERSION} ${arch} ${ANDROID_COMPILER} || exit 1
+		make || exit 1
+		sudo make install || exit 1
+	done
+}
+
 case "$1" in
+	android)   doandroid ;;
 	clean)     doclean ;;
 	dist)      dodist ;;
 	help|*)
@@ -22,6 +36,7 @@ case "$1" in
 
 where <command> is one of:
 
+    android        Build the library for android.
     clean          Clean the build tree and generated files.
     dist           Create (and test) a distribution source tarball.
     help           Show this help screen.
