b95512424e4096b094f16a816e857b43d748cacf
Reece H. Dunn <msclrhd@gmail.com>
2013-05-24 20:58:54 +0100

    phoneme: move the phoneme reader code out into a separate cpp file

---
+++ src/libcainteoir/tts/phoneme_reader.cpp
@@ -0,0 +1,163 @@
+/* Phoneme Reader.
+ *
+ * Copyright (C) 2013 Reece H. Dunn
+ *
+ * This file is part of cainteoir-engine.
+ *
+ * cainteoir-engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cainteoir-engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cainteoir-engine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+#include "i18n.h"
+#include "compatibility.hpp"
+
+#include <cainteoir/phoneme.hpp>
+#include <utility>
+
+namespace tts = cainteoir::tts;
+
+tts::phoneme_reader::phoneme_reader()
+	: phoneme(tts::feature::unspecified, tts::feature::unspecified, tts::feature::unspecified)
+{
+}
+
+struct explicit_feature_reader : public tts::phoneme_reader
+{
+	void reset(const cainteoir::buffer &aBuffer);
+
+	bool read();
+
+	enum state_t
+	{
+		begin_phoneme,
+		in_phoneme,
+		in_feature,
+	};
+
+	const char *mCurrent;
+	const char *mEnd;
+};
+
+void explicit_feature_reader::reset(const cainteoir::buffer &aBuffer)
+{
+	mCurrent = aBuffer.begin();
+	mEnd = aBuffer.end();
+}
+
+bool explicit_feature_reader::read()
+{
+	state_t s = begin_phoneme;
+	char abbrev[4] = { '\0', '\0', '\0', '\0' };
+	int abbrev_pos = 0;
+	tts::feature features[5] = {
+		tts::feature::unspecified,
+		tts::feature::unspecified,
+		tts::feature::unspecified,
+		tts::feature::unspecified,
+		tts::feature::unspecified,
+	};
+	int feature_pos = 0;
+
+	while (mCurrent < mEnd)
+	{
+		switch (s)
+		{
+		case begin_phoneme:
+			switch (*mCurrent)
+			{
+			case '{':
+				s = in_phoneme;
+				break;
+			case '\0':
+				break;
+			default:
+				throw tts::phoneme_error(i18n("unexpected start of phoneme (expecting '{')"));
+			}
+			break;
+		case in_phoneme:
+			switch (*mCurrent)
+			{
+			case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+			case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm':
+			case 'n': case 'o': case 'p': case 'q': case 'r': case 's':
+			case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+				s = in_feature;
+				abbrev_pos = 0;
+				abbrev[abbrev_pos] = *mCurrent;
+				break;
+			case ',': case '}':
+				throw tts::phoneme_error(i18n("missing phoneme feature"));
+			default:
+				throw tts::phoneme_error(i18n("invalid phoneme feature character (only [a-z] supported)"));
+			}
+			break;
+		case in_feature:
+			switch (*mCurrent)
+			{
+			case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+			case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm':
+			case 'n': case 'o': case 'p': case 'q': case 'r': case 's':
+			case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+				++abbrev_pos;
+				if (abbrev_pos <= 2)
+					abbrev[abbrev_pos] = *mCurrent;
+				break;
+			case ',': case '}':
+				if (abbrev_pos != 2)
+					throw tts::phoneme_error(i18n("a phoneme feature must be 3 characters long"));
+
+				if (feature_pos < 5)
+				{
+					auto match = tts::get_feature_id(abbrev);
+					if (!match.first)
+						throw tts::phoneme_error(i18n("unknown phoneme feature"));
+					if (match.second != tts::feature::unspecified)
+						features[feature_pos++] = match.second;
+				}
+				else
+					throw tts::phoneme_error(i18n("a phoneme must specify no more than 5 features"));
+
+				if (*mCurrent == ',')
+					s = in_phoneme;
+				else
+				{
+					if (feature_pos <= 2)
+						throw tts::phoneme_error(i18n("a phoneme must specify at least 3 features"));
+
+					*(tts::phoneme *)this = { features[0], features[1], features[2], features[3], features[4] };
+					++mCurrent;
+					return true;
+				}
+				break;
+			default:
+				throw tts::phoneme_error(i18n("invalid phoneme feature character (only [a-z] supported)"));
+			}
+			break;
+		}
+		++mCurrent;
+	}
+
+	if (s != begin_phoneme)
+		throw tts::phoneme_error(i18n("unexpected end of phoneme (expecting '}')"));
+
+	*(tts::phoneme *)this = { features[0], features[1], features[2], features[3], features[4] };
+	return false;
+}
+
+std::shared_ptr<tts::phoneme_reader> tts::createPhonemeReader(const char *aPhonemeSet)
+{
+	if (!aPhonemeSet) return std::make_shared<explicit_feature_reader>();
+
+	return std::make_shared<explicit_feature_reader>();
+}
