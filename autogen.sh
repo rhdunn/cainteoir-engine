51aaf29d8f91ad4f2634bbf5045380ede1f38574
Reece H. Dunn <msclrhd@gmail.com>
2013-09-28 16:24:06 +0000

    autogen.sh: use verbose to log the files linked using ln

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mkdir -p m4
-ln -sf README.md README
-ln -sf docs/ReleaseNotes.md NEWS
+ln -svf README.md README
+ln -svf docs/ReleaseNotes.md NEWS
 
 if [ -d .git ] ; then
 	git submodule update --init --recursive || exit 1
