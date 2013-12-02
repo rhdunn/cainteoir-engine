3557b7d1f83f051fb56e2d5d9dfce799ec262db7
Reece H. Dunn <msclrhd@gmail.com>
2013-12-02 19:06:41 +0000

    letter_to_phoneme: remove the logic from ruleset
    This removes the existing logic that parsed a ruleset file and
    perfomed rule matching on the text buffer to generate phonemes
    according to the specified letter-to-phoneme rules.
    
    The rationale for the removal is that the file structure and
    processing logic was half-baked. As given, it would be difficult
    to make the necessary improvements to the codebase to support
    rulesets such as those from NRL Report 7948 or eSpeak.
    
    The revised approach is to work on the data structure and
    algorithm first -- starting with the NRL Report 7948 rules -- and
    then working on the file format.

--- src/libcainteoir/tts/letter_to_phoneme.cpp
+++ src/libcainteoir/tts/letter_to_phoneme.cpp
@@ -25,195 +25,32 @@
 
 namespace tts = cainteoir::tts;
 
-static bool left_match(const std::shared_ptr<cainteoir::buffer> &pattern, const char *context)
-{
-	if (!pattern.get())
-		return true;
-
-	return false;
-}
-
-static bool right_match(const std::shared_ptr<cainteoir::buffer> &pattern, const char *context)
-{
-	if (!pattern.get())
-		return true;
-
-	return false;
-}
-
-static const char *mid_match(const std::shared_ptr<cainteoir::buffer> &pattern, const char *context)
-{
-	for (auto c : *pattern)
-	{
-		if (c != *context)
-			return nullptr;
-		++context;
-	}
-	return context;
-}
-
 struct ruleset : public tts::phoneme_reader
 {
-	struct rule_t
-	{
-		const std::shared_ptr<cainteoir::buffer> left;
-		const std::shared_ptr<cainteoir::buffer> match;
-		const std::shared_ptr<cainteoir::buffer> right;
-		const tts::phoneme phoneme;
-	};
-
 	ruleset();
-
-	void add_rule(const std::shared_ptr<cainteoir::buffer> &aLeft,
-	              const std::shared_ptr<cainteoir::buffer> &aMatch,
-	              const std::shared_ptr<cainteoir::buffer> &aRight,
-	              const tts::phoneme aPhoneme);
-
-	bool add_rules(const char *aRulesPath);
 
 	void reset(const std::shared_ptr<cainteoir::buffer> &aBuffer);
 
 	bool read();
 private:
-	void add_rules(const std::shared_ptr<cainteoir::buffer> &aDictionary);
-
-	std::list<rule_t> mRules[256];
 	std::shared_ptr<cainteoir::buffer> mBuffer;
-	const char *mFirst;
-	const char *mCurrent;
-	const char *mLast;
 };
 
 ruleset::ruleset()
-	: mFirst(nullptr)
-	, mCurrent(nullptr)
-	, mLast(nullptr)
 {
-}
-
-void ruleset::add_rule(const std::shared_ptr<cainteoir::buffer> &aLeft,
-                       const std::shared_ptr<cainteoir::buffer> &aMatch,
-                       const std::shared_ptr<cainteoir::buffer> &aRight,
-                       const tts::phoneme aPhoneme)
-{
-	mRules[*aMatch->begin()].push_back({ aLeft, aMatch, aRight, aPhoneme });
-}
-
-bool ruleset::add_rules(const char *aRulesPath)
-{
-	try
-	{
-		add_rules(cainteoir::make_file_buffer(aRulesPath));
-	}
-	catch (const std::exception &)
-	{
-		return false;
-	}
-	return true;
-}
-
-void ruleset::add_rules(const std::shared_ptr<cainteoir::buffer> &aRules)
-{
-	auto phonemes = tts::createPhonemeReader("ipa");
-
-	const char *current = aRules->begin();
-	const char *last    = aRules->end();
-	while (current != last)
-	{
-		if (*current == '#' || *current == '.')
-		{
-			while (current != last && *current != '\r' && *current != '\n')
-				++current;
-
-			while (current != last && (*current == '\r' || *current == '\n'))
-				++current;
-
-			continue;
-		}
-
-		const char *begin_entry = current;
-		const char *end_entry   = current;
-		while (end_entry != last && *end_entry != '\t')
-			++end_entry;
-
-		current = end_entry;
-		while (current != last && *current == '\t')
-			++current;
-
-		if (current == last) return;
-
-		const char *begin_phonemes = current;
-		const char *end_phonemes   = current;
-		while (end_phonemes != last && *end_phonemes != '\r' && *end_phonemes != '\n')
-			++end_phonemes;
-
-		current = end_phonemes;
-		while (current != last && (*current == '\r' || *current == '\n'))
-			++current;
-
-		if (*begin_phonemes == '/' && *(end_phonemes - 1) == '/')
-		{
-			++begin_phonemes;
-			--end_phonemes;
-			phonemes->reset(std::make_shared<cainteoir::buffer>(begin_phonemes, end_phonemes));
-
-			phonemes->read();
-			add_rule(std::shared_ptr<cainteoir::buffer>(), // left
-			         cainteoir::make_buffer(begin_entry, end_entry - begin_entry),
-			         std::shared_ptr<cainteoir::buffer>(), // right
-			         *phonemes);
-		}
-	}
 }
 
 void ruleset::reset(const std::shared_ptr<cainteoir::buffer> &aBuffer)
 {
 	mBuffer = aBuffer;
-	if (mBuffer.get())
-	{
-		mFirst = mCurrent = mBuffer->begin();
-		mLast = mBuffer->end();
-	}
-	else
-	{
-		mFirst = mCurrent = mLast = nullptr;
-	}
 }
 
 bool ruleset::read()
 {
-	while (mCurrent != mLast)
-	{
-		for (auto &rule : mRules[*mCurrent])
-		{
-			const char *s = mid_match(rule.match, mCurrent);
-			if (s == nullptr)
-				continue;
-
-			if (!left_match(rule.left, mCurrent))
-				continue;
-
-			if (!right_match(rule.right, s))
-				continue;
-
-			*(tts::phoneme *)this = rule.phoneme;
-			mCurrent = s;
-			return true;
-		}
-
-		fprintf(stderr, "error: cannot pronounce \"%s\" ... no matching rule for '%c' found.\n",
-		        std::string(mFirst, mLast).c_str(),
-		        *mCurrent);
-		fprintf(stderr, "                         %*c\n", (mCurrent - mFirst + 1), '^');
-		++mCurrent;
-	}
 	return false;
 }
 
 std::shared_ptr<tts::phoneme_reader> tts::createPronunciationRules(const char *aRuleSetPath)
 {
-	auto rules = std::make_shared<ruleset>();
-	if (!rules->add_rules(aRuleSetPath))
-		return {};
-	return rules;
+	return std::make_shared<ruleset>();
 }
