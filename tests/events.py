1e6ab884ad5d98c1e9f8cd38c4707a5dae122fea
Reece H. Dunn <msclrhd@gmail.com>
2013-12-16 16:31:16 +0000

    html: modify the epub3nav linear test to include toc-entry events

--- tests/events.py
+++ tests/events.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
 		{'name': 'epub3 nav', 'type': 'events', 'tests': [
 			# These are tests for HTML5-based ePub3 navigation (table of content)
 			# documents when parsed as HTML (i.e. not embedded in an ePub document).
-			{'test': 'html/epub3nav/linear.xhtml', 'result': 'html/epub3nav/linear.events'},
+			{'test': 'html/epub3nav/linear.xhtml', 'result': 'html/epub3nav/linear.events', 'expect': 'fail'},
 		]},
 	]})
 	test.run({'name': 'MIME', 'replace': ['title'], 'groups': [
