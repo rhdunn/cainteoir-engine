rhdunn/cainteoir-engine

commit b495824808a053b70346e486252102f206c73d30

Redesign the body metadata processing API
The enumeration for controlling whether to process document body
metadata or not via the createDocumentReader function has multiple
issues:

  1.  It does not work well with the single-pass document parser
      implementation approach.

  2.  It is difficult for a document reader to implement.

  3.  The best implementation approach is to use an internal
      version of the redesigned API.

  4.  Proper functionality of document references (page lists, etc.)
      requires full metadata which would require multiple passes
      of the document using the previous API.

The new API resolves all of these issues.

src/libcainteoir/parsers/rtf.cpp
@@ -264,9 +264,7 @@ struct rtf_document_reader : public cainteoir::document_reader
 
 	rtf_document_reader(std::shared_ptr<cainteoir::buffer> &aData, const rdf::uri &aSubject, rdf::graph &aPrimaryMetadata, const std::string &aTitle);
 
-	bool read();
-
-	bool internal_read(rdf::graph *aPrimaryMetadata);
+	bool read(rdf::graph *aMetadata);
 
 	rtf_reader rtf;
 	std::shared_ptr<cainteoir::buffer> mData;
@@ -287,17 +285,12 @@ rtf_document_reader::rtf_document_reader(std::shared_ptr<cainteoir::buffer> &aDa
 	, mBlockCount(0)
 	, mTitle(aTitle)
 {
-	if (rtf.read() && internal_read(&aPrimaryMetadata))
+	if (rtf.read() && read(&aPrimaryMetadata))
 		mState = state_title;
 	aPrimaryMetadata.statement(aSubject, rdf::tts("mimetype"), rdf::literal("application/rtf"));
 }
 
-bool rtf_document_reader::read()
-{
-	return internal_read(nullptr);
-}
-
-bool rtf_document_reader::internal_read(rdf::graph *aGraph)
+bool rtf_document_reader::read(rdf::graph *aGraph)
 {
 	if (mState == state_title)
 	{
@@ -421,7 +414,7 @@ text_event:
 	styles  = &cainteoir::paragraph;
 	content = rtf_text.buffer();
 	anchor  = rdf::uri();
-	if (aGraph == nullptr)
+	if (mState != state_rtf)
 		rtf_text.clear();
 	return true;
 }

docs/api/src/document.dox
@@ -109,11 +109,17 @@ Objects of this type are created by instances of the document_reader interface.
 \brief Destroy the document reader object.
 
 
-\fn    bool cainteoir::document_reader::read()
+\fn    bool cainteoir::document_reader::read(rdf::graph *aMetadata)
 \brief Read the next event in the document.
+
+\param[in] aMetadata Any additional metadata.
 
 \retval true  If an event was read.
 \retval false If there are no more events in the document.
+
+If aMetadata is not null, any metadata gathered while reading the next event is
+added to the metadata graph. This allows metadata in the body of the document
+to be extracted, such as RDFa or document reference metadata.
 
 
 \addtogroup document_reader
@@ -136,23 +142,6 @@ Objects of this type are created by instances of the document_reader interface.
 \addtogroup document_reader
 \{
 
-\enum  cainteoir::metadata
-\brief The amount of metadata to extract from the document.
-
-\}
-
-
-\var   cainteoir::metadata cainteoir::metadata::header_only
-\brief Only extract the metadata contained in the header section of the document.
-
-
-\var   cainteoir::metadata cainteoir::metadata::all
-\brief Extract all the metadata contained in the document.
-
-
-\addtogroup document_reader
-\{
-
 
 \fn    void cainteoir::supportedDocumentFormats(rdf::graph &metadata, capability_types capabilities)
 \brief Get the document formats that are supported by libcainteoir.
@@ -161,14 +150,13 @@ Objects of this type are created by instances of the document_reader interface.
 \param[in]  capabilities The document capabilities to query for.
 
 
-\fn    std::shared_ptr<cainteoir::document_reader> cainteoir::createDocumentReader(const char *aFilename, rdf::graph &aPrimaryMetadata, const std::string &aTitle, const char *aDefaultEncoding, metadata aMetadata)
+\fn    std::shared_ptr<cainteoir::document_reader> cainteoir::createDocumentReader(const char *aFilename, rdf::graph &aPrimaryMetadata, const std::string &aTitle, const char *aDefaultEncoding)
 \brief Create a document content reader.
 
 \param[in]  aFilename        The path to the document.
 \param[out] aPrimaryMetadata The main metadata that describes the document.
 \param[in]  aTitle           The document title to use if none is specified.
 \param[in]  aDefaultEncoding The default character encoding to use.
-\param[in]  aMetadata        How much metadata to extract.
 
 \return A reader over the document contents, or a null pointer if the document is not supported.
 
@@ -179,13 +167,12 @@ The top-level ToC entry is determined as follows (in order of preference):
    -  the title specified in aTitle;
    -  the filename of the document.
 
-If aMetadata is metadata::all, document events for the text are not guaranteed
-to be produced. This is because the parsers perform a single-pass over the
-document and document formats that support this option will scan the entire
-document for metadata.
+After the call to this method, aPrimaryMetadata contains the metadata located
+in the header section of the document. That is the part of the document before
+the start of any document text.
 
 
-\fn    std::shared_ptr<cainteoir::document_reader> cainteoir::createDocumentReader(std::shared_ptr<buffer> &aData, const rdf::uri &aSubject, rdf::graph &aPrimaryMetadata, const std::string &aTitle, const char *aDefaultEncoding, metadata aMetadata)
+\fn    std::shared_ptr<cainteoir::document_reader> cainteoir::createDocumentReader(std::shared_ptr<buffer> &aData, const rdf::uri &aSubject, rdf::graph &aPrimaryMetadata, const std::string &aTitle, const char *aDefaultEncoding)
 \brief Create a document content reader.
 
 \param[in]  aData            The document content.
@@ -193,7 +180,6 @@ document for metadata.
 \param[out] aPrimaryMetadata The main metadata that describes the document.
 \param[in]  aTitle           The document title to use if none is specified.
 \param[in]  aDefaultEncoding The default character encoding to use.
-\param[in]  aMetadata        How much metadata to extract.
 
 \return A reader over the document contents, or a null pointer if the document is not supported.
 
@@ -201,6 +187,10 @@ The top-level ToC entry is determined as follows (in order of preference):
    -  the title specified by the document;
    -  the title specified in aTitle;
    -  the filename of the document.
+
+After the call to this method, aPrimaryMetadata contains the metadata located
+in the header section of the document. That is the part of the document before
+the start of any document text.
 
 
 \fn    std::shared_ptr<cainteoir::document_reader> cainteoir::createDocumentReader(const cainteoir::document::range_type &aDocumentRange)

src/apps/metadata.cpp
@@ -49,12 +49,12 @@ int main(int argc, char ** argv)
 		} output_type = rdf_ntriple;
 
 		bool print_time = false;
-		cainteoir::metadata level = cainteoir::metadata::header_only;
+		bool all_metadata = false;
 
 		const option_group general_options = { nullptr, {
 			{ 't', "time", bind_value(print_time, true),
 			  i18n("Time how long it takes to extract the metadata") },
-			{ 'a', "all", bind_value(level, cainteoir::metadata::all),
+			{ 'a', "all", bind_value(all_metadata, true),
 			  i18n("Extract all available metadata") },
 		}};
 
@@ -86,9 +86,11 @@ int main(int argc, char ** argv)
 			cainteoir::createDocumentReader(nullptr, metadata, std::string());
 		else for(int i = 0; i < argc; ++i)
 		{
-			auto reader = cainteoir::createDocumentReader(argv[i], metadata, std::string(), "windows-1252", level);
+			auto reader = cainteoir::createDocumentReader(argv[i], metadata, std::string());
 			if (!reader)
 				fprintf(stderr, i18n("unsupported document format for file \"%s\"\n"), argv[i]);
+			if (all_metadata)
+				while (reader->read(&metadata)) ;
 		}
 
 		if (print_time)

src/include/cainteoir/document.hpp
@@ -58,7 +58,7 @@ namespace cainteoir
 
 	struct document_reader : public document_item
 	{
-		virtual bool read() = 0;
+		virtual bool read(rdf::graph *aMetadata=nullptr) = 0;
 
 		virtual ~document_reader() {}
 	};
@@ -102,28 +102,20 @@ namespace cainteoir
 		text_support = 2,
 	};
 
-	enum class metadata
-	{
-		header_only,
-		all,
-	};
-
 	void supportedDocumentFormats(rdf::graph &metadata, capability_types capabilities);
 
 	std::shared_ptr<document_reader>
 	createDocumentReader(const char *aFilename,
 	                     rdf::graph &aPrimaryMetadata,
 	                     const std::string &aTitle = std::string(),
-	                     const char *aDefaultEncoding = "windows-1252",
-	                     metadata aMetadata = metadata::header_only);
+	                     const char *aDefaultEncoding = "windows-1252");
 
 	std::shared_ptr<document_reader>
 	createDocumentReader(std::shared_ptr<buffer> &aData,
 	                     const rdf::uri &aSubject,
 	                     rdf::graph &aPrimaryMetadata,
 	                     const std::string &aTitle = std::string(),
-	                     const char *aDefaultEncoding = "windows-1252",
-	                     metadata aMetadata = metadata::header_only);
+	                     const char *aDefaultEncoding = "windows-1252");
 
 	std::shared_ptr<document_reader>
 	createDocumentReader(const document::range_type &aDocumentRange);

src/libcainteoir/document.cpp
@@ -91,13 +91,13 @@ struct document_range : public cainteoir::document_reader
 	{
 	}
 
-	bool read();
+	bool read(rdf::graph *aMetadata);
 
 	cainteoir::document::const_iterator mCurrent;
 	cainteoir::document::const_iterator mLast;
 };
 
-bool document_range::read()
+bool document_range::read(rdf::graph *aMetadata)
 {
 	if (mCurrent == mLast) return false;
 

src/libcainteoir/parsers/document.cpp
@@ -46,8 +46,7 @@ cainteoir::createDocumentReader(std::shared_ptr<buffer> &aData,
                                 const rdf::uri &aSubject,
                                 rdf::graph &aPrimaryMetadata,
                                 const std::string &aTitle,
-                                const char *aDefaultEncoding,
-                                metadata aMetadata)
+                                const char *aDefaultEncoding)
 {
 	if (!aData || aData->empty())
 		return std::shared_ptr<document_reader>();
@@ -142,8 +141,7 @@ std::shared_ptr<cainteoir::document_reader>
 cainteoir::createDocumentReader(const char *aFilename,
                                 rdf::graph &aPrimaryMetadata,
                                 const std::string &aTitle,
-                                const char *aDefaultEncoding,
-                                metadata aMetadata)
+                                const char *aDefaultEncoding)
 {
 	const rdf::uri subject = rdf::uri(aFilename ? aFilename : "stdin", std::string());
 
@@ -153,7 +151,7 @@ cainteoir::createDocumentReader(const char *aFilename,
 	else
 		data = cainteoir::make_file_buffer(stdin);
 
-	return createDocumentReader(data, subject, aPrimaryMetadata, aTitle, aDefaultEncoding, aMetadata);
+	return createDocumentReader(data, subject, aPrimaryMetadata, aTitle, aDefaultEncoding);
 }
 
 void cainteoir::supportedDocumentFormats(rdf::graph &metadata, capability_types capabilities)

src/libcainteoir/parsers/epub.cpp
@@ -44,7 +44,7 @@ struct epub_document_reader : public cainteoir::document_reader
 {
 	epub_document_reader(std::shared_ptr<cainteoir::archive> &aData, const rdf::uri &aSubject, rdf::graph &aPrimaryMetadata, const char *aDefaultEncoding);
 
-	bool read();
+	bool read(rdf::graph *aMetadata);
 
 	cainteoir::path opf_file;
 	cainteoir::path opf_root;
@@ -100,7 +100,7 @@ epub_document_reader::epub_document_reader(std::shared_ptr<cainteoir::archive> &
 	}
 }
 
-bool epub_document_reader::read()
+bool epub_document_reader::read(rdf::graph *aMetadata)
 {
 	while (true) switch (mState)
 	{

src/libcainteoir/parsers/ncx.cpp
@@ -72,7 +72,7 @@ struct ncx_document_reader : public cainteoir::document_reader
 {
 	ncx_document_reader(const std::shared_ptr<xml::reader> &aReader, const rdf::uri &aSubject, rdf::graph &aPrimaryMetadata, const std::string &aTitle);
 
-	bool read();
+	bool read(rdf::graph *aMetadata);
 
 	std::shared_ptr<xml::reader> reader;
 	rdf::uri mSubject;
@@ -80,7 +80,7 @@ struct ncx_document_reader : public cainteoir::document_reader
 	int mDepth;
 };
 
-bool ncx_document_reader::read()
+bool ncx_document_reader::read(rdf::graph *aMetadata)
 {
 	if (!mTitle.empty())
 	{

commit 4d6ba10bc610c1d3ade05d2b751566a9956277d9

rtf: fix event generation (regression from b495824808a053b70346e486252102f206c73d30)

src/libcainteoir/parsers/rtf.cpp
@@ -274,6 +274,7 @@ struct rtf_document_reader : public cainteoir::document_reader
 	int mBlockCount;
 	std::string mTitle;
 	cainteoir::rope rtf_text;
+	bool mClearText;
 };
 
 rtf_document_reader::rtf_document_reader(std::shared_ptr<cainteoir::buffer> &aData, const rdf::uri &aSubject, rdf::graph &aPrimaryMetadata, const std::string &aTitle)
@@ -284,6 +285,7 @@ rtf_document_reader::rtf_document_reader(std::shared_ptr<cainteoir::buffer> &aDa
 	, mState(state_rtf)
 	, mBlockCount(0)
 	, mTitle(aTitle)
+	, mClearText(false)
 {
 	if (rtf.read() && read(&aPrimaryMetadata))
 		mState = state_title;
@@ -312,7 +314,10 @@ bool rtf_document_reader::read(rdf::graph *aGraph)
 
 	std::string info_context;
 	if (!rtf.data()->compare("par") && !rtf_text.empty())
+	{
+		mClearText = true;
 		goto text_event;
+	}
 
 	do switch (rtf.token())
 	{
@@ -414,7 +419,7 @@ text_event:
 	styles  = &cainteoir::paragraph;
 	content = rtf_text.buffer();
 	anchor  = rdf::uri();
-	if (mState != state_rtf)
+	if (mClearText)
 		rtf_text.clear();
 	return true;
 }