51929444fa5404e53659f0507c34374a96f580cc
Reece H. Dunn <msclrhd@gmail.com>
2014-01-23 22:19:16 +0000

    mkapidocs.py: parse all doxygen XML nodes

--- docs/mkapidocs.py
+++ docs/mkapidocs.py
@@ -7,6 +7,10 @@ from xml.dom import minidom
 
 yes_no = {'yes': True , 'no': False, '': False}
 
+
+##### XML API
+
+
 class XmlNode:
 	def __init__(self, node):
 		self.node = node
@@ -38,178 +42,135 @@ class XmlDocument(XmlNode):
 		XmlNode.__init__(self, minidom.parse(filename).documentElement)
 
 
-class Member:
-	def __init__(self, ref):
-		self.ref = ref
-		self.kind = None
+##### Doxygen XML Parser
 
-	def parse(self, xml, references):
-		self.kind = xml['kind']
 
-	def output(self):
-		print('   (%s)' % (self.kind))
+class Item:
+	def __init__(self, ref, kind, name):
+		self.ref  = ref
+		self.kind = kind
+		self.name = name
 
 	def __str__(self):
-		return '(%s)' % self.kind
+		return str((self.ref, self.kind, self.name))
 
 
-class Variable(Member):
-	def __init__(self, ref):
-		Member.__init__(self, ref)
-		self.protection = None
-		self.static = None
-		self.mutable = None
-		self.vartype = None
-		self.name = None
-
-	def parse(self, xml, references):
-		self.kind = xml['kind']
-		self.protection = xml['prot']
-		self.static = yes_no[xml['static']]
-		self.mutable = yes_no[xml['mutable']]
-		for child in xml:
-			if child.name == 'type':
-				self._parseType(child, references)
-			elif child.name == 'name':
-				self.name = child.text()
-
-	def _parseType(self, xml, references):
-		for child in xml:
-			if child.name == 'ref':
-				self.vartype = references.create(child)
-		if not self.vartype:
-			self.vartype = xml.text()
-
-	def output(self):
-		print('   %s %s %s' % (self.protection, self.vartype, self.name))
-
-
-class Function(Member):
-	def __init__(self, ref):
-		Member.__init__(self, ref)
-
-
-class BaseCompound:
-	def __init__(self, base):
-		self.base = base
-		self.protection = None
-		self.virtual = None
-
-	def parse(self, xml, references):
-		self.protection = xml['prot']
-		self.virtual = xml['virt']
-
-
-class Compound:
-	def __init__(self, ref):
-		self.ref = ref
-		self.name = None
-		self.kind = None
-		self.protection = None
-		self.parents = []
-		self.members = []
-
-	def __str__(self):
-		if self.name:
-			return self.name
-		return '[%s]' % self.ref
-
-	def parse(self, xml, references):
-		self.kind = xml['kind']
-		self.protection = xml['prot']
-		for child in xml:
-			if child.name == 'compoundname':
-				self.name = child.text()
-			elif child.name == 'basecompoundref':
-				b = references.create(child)
-				if b:
-					self.parents.append(b)
-			elif child.name == 'sectiondef':
-				self._parseSectionDef(child, references)
-
-	def _parseSectionDef(self, xml, references):
-		for child in xml:
-			if child.name == 'memberdef':
-				m = references.create(child)
-				if m:
-					self.members.append(m)
-
-	def output(self):
-		print('%s %s %s' % (self.protection, self.kind, self.name))
-		for p in self.parents:
-			print('   : %s %s' % (p.protection, p.base))
-		print('{')
-		for m in self.members:
-			m.output()
-		print('}')
-		print('')
-
-
-class References:
-	def __init__(self):
-		self.object_types = { #     ref-type      val-type
-			'basecompoundref': (BaseCompound, Compound),
-			'compounddef':     (Compound,     Compound),
-			'compound':        (Compound,     Compound),           # ref|@kindref
-			'function':        (Function,     Function),           # memberdef|@kind
-			'memberdef':       (Member,       Member),
-			'member':          (Member,       Member),
-			'variable':        (Variable,     Variable),           # memberdef|@kind
-		}
-		self.items = {}
-
-	def create(self, xml):
-		if xml.name == 'ref':
-			kind = xml['kindref']
-			ref  = xml['refid']
-			if not ref in self.items.keys():
-				ref_type, val_type = self.object_types[kind]
-				obj = val_type(ref)
-				self.items[ref] = obj
-			return self.items[ref]
-
-		kind = xml['kind']
-		if kind in self.object_types.keys():
-			ref_type, val_type = self.object_types[kind]
+def parseDoxygenXml_memberdef_enum(xml, compound):
+	for child in xml:
+		if child.name == 'name':
+			member = Item(xml['id'], xml['kind'], child.text())
+			print('... %s' % member)
+		elif child.name in ['enumvalue', 'briefdescription', 'detaileddescription', 'inbodydescription', 'location']:
+			pass
 		else:
-			if not xml.name in self.object_types.keys():
-				return None
-			ref_type, val_type = self.object_types[xml.name]
+			raise Exception('Unknown memberdef node : %s' % child.name)
 
-		ref = xml['id']
-		if ref:
-			obj = val_type(ref)
-			if not ref in self.items.keys():
-				self.items[ref] = val_type(ref)
-			obj = self.items[ref]
-			obj.parse(xml, self)
+
+def parseDoxygenXml_memberdef_typedef(xml, compound):
+	for child in xml:
+		if child.name == 'name':
+			member = Item(xml['id'], xml['kind'], child.text())
+			print('... %s' % member)
+		elif child.name in ['type', 'definition', 'argsstring', 'briefdescription', 'detaileddescription', 'inbodydescription', 'location']:
+			pass
 		else:
-			ref = xml['refid']
-			if not ref:
-				return None
-			if not ref in self.items.keys():
-				self.items[ref] = val_type(ref)
-			obj = ref_type(self.items[ref])
-			obj.parse(xml, self)
-		return obj
-
-	def __iter__(self):
-		for item in self.items.values():
-			yield item
+			raise Exception('Unknown memberdef node : %s' % child.name)
 
 
-def parseDoxygenXml(element, references):
-	compounds = []
-	for child in element:
-		compounds.append(references.create(child))
-	return compounds
+def parseDoxygenXml_memberdef_variable(xml, compound):
+	for child in xml:
+		if child.name == 'name':
+			member = Item(xml['id'], xml['kind'], child.text())
+			print('... %s' % member)
+		elif child.name in ['type', 'definition', 'argsstring', 'briefdescription', 'detaileddescription', 'inbodydescription', 'location']:
+			pass
+		else:
+			raise Exception('Unknown memberdef node : %s' % child.name)
 
 
-references = References()
+def parseDoxygenXml_memberdef_function(xml, compound):
+	for child in xml:
+		if child.name == 'name':
+			member = Item(xml['id'], xml['kind'], child.text())
+			print('... %s' % member)
+		elif child.name in ['type', 'definition', 'argsstring', 'param', 'templateparamlist', 'briefdescription', 'detaileddescription', 'inbodydescription', 'location', 'reimplements', 'reimplementedby']:
+			pass
+		else:
+			raise Exception('Unknown memberdef node : %s' % child.name)
+
+
+def parseDoxygenXml_sectiondef(xml, compound):
+	for child in xml:
+		if child.name == 'memberdef':
+			if child['kind'] == 'variable':
+				parseDoxygenXml_memberdef_variable(child, compound)
+			elif child['kind'] == 'function':
+				parseDoxygenXml_memberdef_function(child, compound)
+			elif child['kind'] == 'typedef':
+				parseDoxygenXml_memberdef_typedef(child, compound)
+			elif child['kind'] == 'enum':
+				parseDoxygenXml_memberdef_enum(child, compound)
+			else:
+				raise Exception('Unknown memberdef : %s' % child['kind'])
+		elif child.name in ['header']:
+			pass
+		else:
+			raise Exception('Unknown sectiondef node : %s' % child.name)
+
+
+def parseDoxygenXml_compounddef_namespace(xml):
+	for child in xml:
+		if child.name == 'compoundname':
+			compound = Item(xml['id'], xml['kind'], child.text())
+			print(compound)
+		elif child.name == 'sectiondef':
+			parseDoxygenXml_sectiondef(child, compound)
+		elif child.name in ['innerclass', 'innernamespace', 'briefdescription', 'detaileddescription', 'location']:
+			pass
+		else:
+			raise Exception('Unknown compounddef node : %s' % child.name)
+
+
+def parseDoxygenXml_compounddef_class(xml):
+	for child in xml:
+		if child.name == 'compoundname':
+			compound = Item(xml['id'], xml['kind'], child.text())
+			print(compound)
+		elif child.name == 'basecompoundref':
+			parent = Item(xml['refid'], None, child.text())
+			print('   => %s' % parent)
+		elif child.name == 'derivedcompoundref':
+			derived = Item(xml['refid'], None, child.text())
+			print('   <= %s' % derived)
+		elif child.name == 'sectiondef':
+			parseDoxygenXml_sectiondef(child, compound)
+		elif child.name in ['includes', 'briefdescription', 'detaileddescription', 'inheritancegraph', 'collaborationgraph', 'location', 'listofallmembers', 'innerclass', 'templateparamlist']:
+			pass
+		else:
+			raise Exception('Unknown compounddef node : %s' % child.name)
+
+
+def parseDoxygenXml(xml):
+	for child in xml:
+		if child.name == 'compounddef':
+			if child['kind'] in ['class', 'struct']:
+				parseDoxygenXml_compounddef_class(child)
+			elif child['kind'] == 'namespace':
+				parseDoxygenXml_compounddef_namespace(child)
+			elif child['kind'] in ['file', 'dir', 'group', 'page']:
+				pass
+			else:
+				raise Exception('Unknown compounddef : %s' % child['kind'])
+		elif child.name == 'compound':
+			pass
+		else:
+			raise Exception('Unknown doxygen node : %s' % child.name)
+
+
+##### Main Entry Point
+
+
 compounds  = []
 for filename in sys.argv[1:]:
 	doc = XmlDocument(filename)
-	compounds.extend(parseDoxygenXml(doc, references))
-
-for c in compounds:
-	c.output()
+	parseDoxygenXml(doc)
