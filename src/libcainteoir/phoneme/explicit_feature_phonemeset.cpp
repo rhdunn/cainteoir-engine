ed332155ae5a7a21ff88bc28776214cf437f7fe4
Reece H. Dunn <msclrhd@gmail.com>
2014-03-05 10:56:11 +0000

    phonemeset: support the nasal release diacritic

--- src/libcainteoir/phoneme/explicit_feature_phonemeset.cpp
+++ src/libcainteoir/phoneme/explicit_feature_phonemeset.cpp
@@ -93,6 +93,7 @@ static const std::initializer_list<const char *> phoneme_type = {
 
 static const std::initializer_list<const char *> release = {
 	"asp",
+	"nzr",
 };
 
 static const std::initializer_list<const char *> phonation = {
