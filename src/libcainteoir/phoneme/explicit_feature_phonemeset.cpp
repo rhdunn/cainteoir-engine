576ac4cdfc58637b7b15596796feb5b5b1ca947e
Reece H. Dunn <msclrhd@gmail.com>
2014-03-05 11:04:17 +0000

    phonemeset: support the lateral release diacritic

--- src/libcainteoir/phoneme/explicit_feature_phonemeset.cpp
+++ src/libcainteoir/phoneme/explicit_feature_phonemeset.cpp
@@ -94,6 +94,7 @@ static const std::initializer_list<const char *> phoneme_type = {
 static const std::initializer_list<const char *> release = {
 	"asp",
 	"nzr",
+	"ltr",
 };
 
 static const std::initializer_list<const char *> phonation = {
