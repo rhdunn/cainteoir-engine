d8e36b56e8d0a5ace071211a6f77dc875f3a1db6
Reece H. Dunn <msclrhd@gmail.com>
2014-03-09 14:15:14 +0000

    Move tests/phonemeset/{ => ipa/}*

--- tests/phonemeset.py
+++ tests/phonemeset.py
@@ -24,36 +24,36 @@ if __name__ == '__main__':
 	test = harness.TestSuite('phonemeset', sys.argv)
 	test.run({'name': 'Feature-Based', 'groups': [
 		{'name': 'Features', 'type': 'phonemeset', 'tests': [
-			{'test': 'phonemeset/phonemes.features', 'result': 'phonemeset/phonemes.features', 'from': 'features', 'to': 'features'},
-			{'test': 'phonemeset/tied-phonemes.features', 'result': 'phonemeset/tied-phonemes.features', 'from': 'features', 'to': 'features'},
+			{'test': 'phonemeset/ipa/phonemes.features', 'result': 'phonemeset/ipa/phonemes.features', 'from': 'features', 'to': 'features'},
+			{'test': 'phonemeset/ipa/tied-phonemes.features', 'result': 'phonemeset/ipa/tied-phonemes.features', 'from': 'features', 'to': 'features'},
 		]},
 	]})
 	test.run({'name': 'IPA-Based', 'groups': [
 		{'name': 'IPA', 'type': 'phonemeset', 'tests': [
-			{'test': 'phonemeset/phonemes.features', 'result': 'phonemeset/phonemes.ipa', 'from': 'features', 'to': 'ipa'},
-			{'test': 'phonemeset/phonemes.ipa', 'result': 'phonemeset/phonemes.features', 'from': 'ipa', 'to': 'features'},
-			{'test': 'phonemeset/tied-phonemes.features', 'result': 'phonemeset/tied-phonemes.ipa', 'from': 'features', 'to': 'ipa'},
-			{'test': 'phonemeset/tied-phonemes.ipa', 'result': 'phonemeset/tied-phonemes.features', 'from': 'ipa', 'to': 'features'},
+			{'test': 'phonemeset/ipa/phonemes.features', 'result': 'phonemeset/ipa/phonemes.ipa', 'from': 'features', 'to': 'ipa'},
+			{'test': 'phonemeset/ipa/phonemes.ipa', 'result': 'phonemeset/ipa/phonemes.features', 'from': 'ipa', 'to': 'features'},
+			{'test': 'phonemeset/ipa/tied-phonemes.features', 'result': 'phonemeset/ipa/tied-phonemes.ipa', 'from': 'features', 'to': 'ipa'},
+			{'test': 'phonemeset/ipa/tied-phonemes.ipa', 'result': 'phonemeset/ipa/tied-phonemes.features', 'from': 'ipa', 'to': 'features'},
 		]},
 		{'name': 'X-SAMPA', 'type': 'phonemeset', 'tests': [
-			{'test': 'phonemeset/phonemes.features', 'result': 'phonemeset/phonemes.x-sampa', 'from': 'features', 'to': 'x-sampa'},
-			{'test': 'phonemeset/phonemes.x-sampa', 'result': 'phonemeset/phonemes.features', 'from': 'x-sampa', 'to': 'features'},
+			{'test': 'phonemeset/ipa/phonemes.features', 'result': 'phonemeset/ipa/phonemes.x-sampa', 'from': 'features', 'to': 'x-sampa'},
+			{'test': 'phonemeset/ipa/phonemes.x-sampa', 'result': 'phonemeset/ipa/phonemes.features', 'from': 'x-sampa', 'to': 'features'},
 			# NOTE: X-SAMPA has ambiguous transcriptions for diacritics vs ties, so these fail to round-trip
-			{'test': 'phonemeset/tied-phonemes.features', 'result': 'phonemeset/tied-phonemes.x-sampa', 'from': 'features', 'to': 'x-sampa'},
-			{'test': 'phonemeset/tied-phonemes.x-sampa', 'result': 'phonemeset/tied-phonemes.x-sampa-features', 'from': 'x-sampa', 'to': 'features'},
+			{'test': 'phonemeset/ipa/tied-phonemes.features', 'result': 'phonemeset/ipa/tied-phonemes.x-sampa', 'from': 'features', 'to': 'x-sampa'},
+			{'test': 'phonemeset/ipa/tied-phonemes.x-sampa', 'result': 'phonemeset/ipa/tied-phonemes.x-sampa-features', 'from': 'x-sampa', 'to': 'features'},
 		]},
 		{'name': 'CXS', 'type': 'phonemeset', 'tests': [
-			{'test': 'phonemeset/phonemes.features', 'result': 'phonemeset/phonemes.cxs', 'from': 'features', 'to': 'cxs'},
-			{'test': 'phonemeset/phonemes.cxs', 'result': 'phonemeset/phonemes.features', 'from': 'cxs', 'to': 'features'},
+			{'test': 'phonemeset/ipa/phonemes.features', 'result': 'phonemeset/ipa/phonemes.cxs', 'from': 'features', 'to': 'cxs'},
+			{'test': 'phonemeset/ipa/phonemes.cxs', 'result': 'phonemeset/ipa/phonemes.features', 'from': 'cxs', 'to': 'features'},
 			# NOTE: X-SAMPA has ambiguous transcriptions for diacritics vs ties, so these fail to round-trip
-			{'test': 'phonemeset/tied-phonemes.features', 'result': 'phonemeset/tied-phonemes.cxs', 'from': 'features', 'to': 'cxs'},
-			{'test': 'phonemeset/tied-phonemes.cxs', 'result': 'phonemeset/tied-phonemes.cxs-features', 'from': 'cxs', 'to': 'features'},
+			{'test': 'phonemeset/ipa/tied-phonemes.features', 'result': 'phonemeset/ipa/tied-phonemes.cxs', 'from': 'features', 'to': 'cxs'},
+			{'test': 'phonemeset/ipa/tied-phonemes.cxs', 'result': 'phonemeset/ipa/tied-phonemes.cxs-features', 'from': 'cxs', 'to': 'features'},
 		]},
 		{'name': 'ASCII-IPA', 'type': 'phonemeset', 'tests': [
-			{'test': 'phonemeset/phonemes.features', 'result': 'phonemeset/phonemes.ascii-ipa', 'from': 'features', 'to': 'ascii-ipa'},
-			{'test': 'phonemeset/phonemes.ascii-ipa', 'result': 'phonemeset/phonemes.features', 'from': 'ascii-ipa', 'to': 'features'},
-			{'test': 'phonemeset/tied-phonemes.features', 'result': 'phonemeset/tied-phonemes.ascii-ipa', 'from': 'features', 'to': 'ascii-ipa'},
-			{'test': 'phonemeset/tied-phonemes.ascii-ipa', 'result': 'phonemeset/tied-phonemes.features', 'from': 'ascii-ipa', 'to': 'features'},
+			{'test': 'phonemeset/ipa/phonemes.features', 'result': 'phonemeset/ipa/phonemes.ascii-ipa', 'from': 'features', 'to': 'ascii-ipa'},
+			{'test': 'phonemeset/ipa/phonemes.ascii-ipa', 'result': 'phonemeset/ipa/phonemes.features', 'from': 'ascii-ipa', 'to': 'features'},
+			{'test': 'phonemeset/ipa/tied-phonemes.features', 'result': 'phonemeset/ipa/tied-phonemes.ascii-ipa', 'from': 'features', 'to': 'ascii-ipa'},
+			{'test': 'phonemeset/ipa/tied-phonemes.ascii-ipa', 'result': 'phonemeset/ipa/tied-phonemes.features', 'from': 'ascii-ipa', 'to': 'features'},
 		]},
 	]})
 	test.summary()
