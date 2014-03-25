273d2bce077d383b59fc57aa74b2fa34fb33c65a
Reece H. Dunn <msclrhd@gmail.com>
2014-03-25 23:37:50 +0000

    phonemeset: support espeak/en-GB-x-lancastr

--- tests/phonemeset.py
+++ tests/phonemeset.py
@@ -100,5 +100,9 @@ if __name__ == '__main__':
 			{'test': 'phonemeset/espeak/en-GB-scotland.ipa', 'result': 'phonemeset/espeak/en-GB-scotland.espeak', 'from': 'ipa', 'to': 'espeak/en-GB-scotland'},
 			{'test': 'phonemeset/espeak/en', 'result': 'phonemeset/espeak/en-GB-scotland.ipa', 'from': 'espeak/en-GB-scotland', 'to': 'ipa'},
 		]},
+		{'name': 'English (Lancastrian)', 'type': 'phonemeset', 'tests': [
+			{'test': 'phonemeset/espeak/en.ipa', 'result': 'phonemeset/espeak/en.espeak', 'from': 'ipa', 'to': 'espeak/en-GB-x-lancastr'},
+			{'test': 'phonemeset/espeak/en', 'result': 'phonemeset/espeak/en.ipa', 'from': 'espeak/en-GB-x-lancastr', 'to': 'ipa'},
+		]},
 	]})
 	test.summary()
