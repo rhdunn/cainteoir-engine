7b2ed86120361423873de19de57c2661b0157169
Reece H. Dunn <msclrhd@gmail.com>
2014-06-28 14:10:11 +0100

    prosody: test multiple words

--- tests/prosody.py
+++ tests/prosody.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
 	test.run({'name': 'Prosody', 'phonemeset': 'cxs', 'dictionary': 'dict/cainteoir/words.dict', 'groups': [
 		{'name': 'Phoneme Grouping', 'type': 'prosody', 'tests': [
 			{'test': 'prosody/grouping/simple-phonemes.txt', 'result': 'prosody/grouping/simple-phonemes.pho'},
+			{'test': 'prosody/grouping/multiple-words.txt', 'result': 'prosody/grouping/multiple-words.pho'},
 		]},
 	]})
 	test.summary()
